181f17505e0d5ba4d5092b31e3d1d8611ef40398
AmarKajevic <kajevic18@gmail.com>
2023-01-12 10:45:04 +0100

    Add project files.

---
+++ RunGroopWebApp/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RunGroopWebApp.Helpers;
+using RunGroopWebApp.Interfaces;
+using RunGroopWebApp.Models;
+using RunGroopWebApp.ViewModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace RunGroopWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IClubRepository _clubrepository;
+
+        public HomeController(ILogger<HomeController> logger, IClubRepository clubrepository)
+        {
+            _logger = logger;
+            _clubrepository = clubrepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var ipInfo = new IPInfo();
+            var homeViewModel = new HomeViewModel();
+            try
+            {
+                string url = "https://ipinfo.io/109.92.189.118?token=df686a956fc3ca";
+                var info = new WebClient().DownloadString(url);
+                ipInfo = JsonConvert.DeserializeObject<IPInfo>(info);
+                RegionInfo myRI1 = new RegionInfo(ipInfo.Country);
+                ipInfo.Country = myRI1.EnglishName;
+                homeViewModel.City = ipInfo.City;
+                homeViewModel.State = ipInfo.Region;
+                if(homeViewModel.City != null)
+                {
+                    homeViewModel.Clubs = await _clubrepository.GetClubByCity(homeViewModel.City);
+                }
+                else
+                {
+                    homeViewModel.Clubs = null;
+                }
+                return View(homeViewModel);
+            }
+            catch(Exception ex)
+            {
+                homeViewModel.Clubs = null;
+            }
+            return View(homeViewModel);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
